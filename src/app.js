a1ed1e385bf8e546c958382d67527abf2af7e7eb
satyam <smmyself007@gmail.com>
2025-11-16 09:56:13 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(5000, () => {
+    console.log("server is running on port 5000...");
+});
+
+
+ app.use((req,res) => {
+     res.send("Hello fuck yes from the server!");
+ });
+
+// app.listen(3000, () => {
+//     console.log("server is running on port 3000...");
+
+// });
---
+++ src/homeworks2e1.md
@@ -0,0 +1,10 @@
+create a repository
+initialize the repository
+node_modukes,package.json,package-lock.json
+install express
+create a server
+listen to port 7777
+write request handlers for /test, /hello
+install nodemon and update inside package.json
+diff between (~ and ^ )
+what is use of "-g" while npm install
