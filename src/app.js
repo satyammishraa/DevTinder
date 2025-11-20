bbce59f7caf4d9335fcd3074f27282222ade323d
satyam <smmyself007@gmail.com>
2025-11-20 13:11:15 +0530

    post,patch api calls

--- src/app.js
+++ src/app.js
@@ -26,6 +26,51 @@ app.post("/signup", async (req,res) => {
 
  });
 
+ app.get("/feed",async (req,res) =>{
+
+    const useremail = req.body.emailId;
+
+    try{
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("something went wrong ");
+    }
+
+ });
+
+app.delete("/deluser", async (req,res) => {
+    const userId = req.body.userId;
+
+    
+    try{
+
+        const user = await User.findByIdAndDelete(userId);
+
+        res.send("user delete successfully");
+    }
+    catch (err) {
+        res.status(400).send("something went wrong");
+    }
+
+});
+app.patch("/upduser", async (req,res) => {
+    const userId = req.body.userId;
+const data = req.body;
+    
+    try{
+
+        const user = await User.findByIdAndUpdate({_id:userId},data);
+
+        res.send("user updated successfully");
+    }
+    catch (err) {
+        res.status(400).send("something went wrong");
+    }
+
+});
+
+
 
 connectDB()
 .then(() => {
